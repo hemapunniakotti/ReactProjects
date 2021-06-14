fe1e041939e22c72a37c459680e4c6a3f2de168a
hemapunniakotti <30875633+hemapunniakotti@users.noreply.github.com>
2021-06-14 00:28:54 -0400

    Add files via upload

---
+++ Assignment1_IntroReact/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hema's First React Application
+        </p>
+          Yay!!! my application is live :)
+      </header>
+    </div>
+  );
+}
+
+export default App;
